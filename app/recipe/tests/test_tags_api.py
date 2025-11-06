0f0440abeae73d4490afe5c2e3526bd935d260fb
TracyWel <tracy@truthdata.net>
2025-11-06 12:36:57 -0600

    test failing

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -16,6 +16,11 @@ from recipe.serializers import TagSerializer
 
 TAGS_URL = reverse('recipe:tag-list')
 
+
+def detail_url(obj):
+    """Create and return URL for Tag"""
+    return reverse('recipe:tag-detail', args=[tag_id])
+
 def create_user(email='user@example.com', password='password'):
     """Create new user"""
     return get_user_model().objects.create_user(email=email, password=password)
@@ -67,3 +72,16 @@ class PrivateTagsApiTests(TestCase):
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], tag.name)
         self.assertEqual(res.data[0]['id'], tag.id)
+
+    def test_update_tag(self):
+        """Test updating a tag"""
+        tag = Tag.objects.create(user=self.user, name='After dinner')
+        payload = {
+            'name': 'Dessert',
+        }
+        url = detail_url(tag.id)
+        res = self.client.patch(url, data=payload)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        tag.refresh_from_db()
+        self.assertEqual(tag.name, payload['name'])
