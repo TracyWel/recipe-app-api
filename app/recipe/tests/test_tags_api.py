59a63ac04b81fa55e7401d4bfdc96962c7633d29
TracyWel <tracy@truthdata.net>
2025-11-06 15:52:58 -0600

    section 102 test failing

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -17,7 +17,7 @@ from recipe.serializers import TagSerializer
 TAGS_URL = reverse('recipe:tag-list')
 
 
-def detail_url(obj):
+def detail_url(tag_id):
     """Create and return URL for Tag"""
     return reverse('recipe:tag-detail', args=[tag_id])
 
@@ -85,3 +85,13 @@ class PrivateTagsApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         tag.refresh_from_db()
         self.assertEqual(tag.name, payload['name'])
+
+    def test_delete_tag(self):
+        """Test deleting a tag"""
+        tag = Tag.objects.create(user=self.user, name='Breakfast')
+        url = detail_url(tag.id)
+        res = self.client.delete(url)
+
+        self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
+        tags = Tag.objects.filter(user=self.user)
+        self.assertFalse(tags.exists())
